f229e513b908d2a9fe224e134db5666eee29f503
audra <audrone.virbickaite@gmail.com>
2022-08-31 14:08:32 +0200

    r2 changes

--- Rev2_codes/2_run_models_r2OK.R
+++ Rev2_codes/2_run_models_r2OK.R
@@ -1,6 +1,7 @@
 rm(list=ls(all=TRUE))
 library(sfsmisc)
 library(truncnorm)
+library(DMFP)
 
 load('data/10data.Rdata')
 T0  = 1990 
@@ -8,6 +9,10 @@ T0  = 1990
 # the length of the estimation window, 1:T0, which is 
 # from "2001-02-01" till "2008-12-31"
 # the rest will be used for model evaluation
+
+# 250 is MCMC size. The codes have been run for 50k and the results are in
+# Rdata files in folder temp. The files are too large for GitHub
+# they are stored locally 
 
 scalartdcc(stand,250)
 rmetrics(stand,250)
