9df63bee7286c72f6937b78c5d732e1ea23c4b49
Virbickaite <audrone.virbickaite@gmail.com>
2023-11-23 16:45:18 +0100

    d

--- empirical/2_run_XM.R
+++ empirical/2_run_XM.R
@@ -6,6 +6,7 @@ library(matrixcalc)
 library(mixAK)
 library(LaplacesDemon)
 library(countreg)
+library(DMFP)
 
 nn       = length(date)
 end.date = which(zoo::as.yearmon(date)=="ene 2021")[1]-1
@@ -26,5 +27,5 @@ nn
 data = Sigma[1:T0]
 
 
-xm(data,200)
+xm(data,5000)
 
