a98447d99d6ab18ec70efd0d678403959809c9b5
audra <audrone.virbickaite@gmail.com>
2023-12-06 19:43:56 +0100

    km

--- empirical/working_code_vectordcc_tcop.R
+++ empirical/working_code_vectordcc_tcop.R
@@ -1,11 +1,14 @@
 rm(list=ls(all=TRUE))
 load('data/FXdata.Rdata')
 library(matrixcalc)
+library(mvnfast)
 
+library(profvis)
 
+({
 nn       = length(date)
-end.date = which(zoo::as.yearmon(date)=="ene 2021")[1]-1
-if(is.na(date[end.date])){end.date = which(zoo::as.yearmon(date)=="jan 2021")[1]-1}
+end.date = which(zoo::as.yearmon(date)=="ene 2020")[1]-1
+if(is.na(date[end.date])){end.date = which(zoo::as.yearmon(date)=="jan 2020")[1]-1}
 
 date[end.date]
 
@@ -18,16 +21,13 @@ T0+K
 nn
 # the same
 
-data = stand[1:T0,]
-M=5000
+T0=100
+data = stand[1:T0,1:5]
+M=1000
 propsd=0.001
 propsdnu = 0.01
 
-
-
 ###
-
-
 
 t0   = Sys.time()
 TT   = dim(data)[1]
@@ -49,7 +49,8 @@ R[,,1] <- diag(diag(Qold[,,1])^{-1/2})%*%Qold[,,1]%*%diag(diag(Qold[,,1])^{-1/2}
 
 resdcc <- matrix(NA,ncol=dm*2+1,nrow=(bi+M))
 iota   = rep(1,dm)
-B0     = (iota%*%t(iota)-aold%*%t(aold)-bold%*%t(bold))*cov(tdata)
+Sbar   = cov(tdata)
+B0     = (iota%*%t(iota)-aold%*%t(aold)-bold%*%t(bold))*Sbar
 
 accdcc <- rep(0,bi+M)
 accnu  <- rep(0,bi+M)
@@ -58,8 +59,8 @@ Vpred  = Qpred = list()
 for(t in 2:TT){
   Qold[,,t]   <- B0+(aold%*%t(aold))*(tdata[t-1,]%*%t(tdata[t-1,]))+(bold%*%t(bold))*Qold[,,(t-1)]
   R[,,t]   <- diag(diag(Qold[,,t])^{-1/2})%*%Qold[,,t]%*%diag(diag(Qold[,,t])^{-1/2})
-  llold[t] <- mvtnorm::dmvt(tdata[t,], rep(0,dm), R[,,t], df=nuold, log=TRUE)-
-    sum(dt(tdata[t,],df=nuold,log=TRUE))
+  inlik=sum(dt(tdata[t,],df=nuold,log=TRUE))
+  llold[t] <- dmvt(tdata[t,], rep(0,dm), R[,,t], df=nuold, log=TRUE)-inlik
 }
 
 for(m in 1:(M+bi)){
@@ -72,7 +73,7 @@ for(m in 1:(M+bi)){
     bn  = rnorm(dm*2,c(aold,bold),sd=propsd)
     anew = bn[1:dm]
     bnew = bn[(dm+1):(2*dm)]
-    B0  = (iota%*%t(iota)-anew%*%t(anew)-bnew%*%t(bnew))*cov(tdata)
+    B0  = (iota%*%t(iota)-anew%*%t(anew)-bnew%*%t(bnew))*Sbar
     if(anew[1]>0 && bnew[1]>0 && is.positive.definite(B0) && (sum(abs(anew%*%t(anew)+bnew%*%t(bnew))<1)==dm^2)) break
   }
   
@@ -80,10 +81,10 @@ for(m in 1:(M+bi)){
   Qnew  = Qold
   
   for(t in 2:TT){
-    Qnew[,,t]   <- B0+(anew%*%t(anew))*(tdata[t-1,]%*%t(tdata[t-1,]))+(bnew%*%t(bnew))*Qnew[,,(t-1)]
-    R[,,t]   <- diag(diag(Qnew[,,t])^{-1/2})%*%Qnew[,,t]%*%diag(diag(Qnew[,,t])^{-1/2})
-    llnew[t] <- mvtnorm::dmvt(tdata[t,], rep(0,dm), R[,,t], df=nuold, log=TRUE)-
-      sum(dt(tdata[t,],df=nuold,log=TRUE))
+    Qnew[,,t] <- B0+(anew%*%t(anew))*(tdata[t-1,]%*%t(tdata[t-1,]))+(bnew%*%t(bnew))*Qnew[,,(t-1)]
+    R[,,t]    <- diag(diag(Qnew[,,t])^{-1/2})%*%Qnew[,,t]%*%diag(diag(Qnew[,,t])^{-1/2})
+    inlik = sum(dt(tdata[t,],df=nuold,log=TRUE))
+    llnew[t]  <- dmvt(tdata[t,], rep(0,dm), R[,,t], df=nuold, log=TRUE)-inlik
   }
   
   if((sum(llnew)-sum(llold)+
@@ -107,16 +108,18 @@ for(m in 1:(M+bi)){
   }
   
   tdata  = qt(udata,nunew)
-  B0     = (iota%*%t(iota)-aold%*%t(aold)-bold%*%t(bold))*cov(tdata)
+  Sbar   = cov(tdata)
+  B0     = (iota%*%t(iota)-aold%*%t(aold)-bold%*%t(bold))*Sbar
   
   llnew <- rep(0,TT)
   Qnew  = Qold
   
   for(t in 2:TT){
-    Qnew[,,t]   <- B0+(aold%*%t(aold))*(tdata[t-1,]%*%t(tdata[t-1,]))+(bold%*%t(bold))*Qnew[,,(t-1)]
-    R[,,t]   <- diag(diag(Qnew[,,t])^{-1/2})%*%Qnew[,,t]%*%diag(diag(Qnew[,,t])^{-1/2})
-    llnew[t] <- mvtnorm::dmvt(tdata[t,], rep(0,dm), R[,,t], df=nunew, log=TRUE)-
-      sum(dt(tdata[t,],df=nunew,log=TRUE))
+    Qnew[,,t] <- B0+(aold%*%t(aold))*(tdata[t-1,]%*%t(tdata[t-1,]))+(bold%*%t(bold))*Qnew[,,(t-1)]
+    R[,,t]    <- diag(diag(Qnew[,,t])^{-1/2})%*%Qnew[,,t]%*%diag(diag(Qnew[,,t])^{-1/2})
+    inlik=sum(dt(tdata[t,],df=nunew,log=TRUE))
+    llnew[t]  <- dmvt(tdata[t,], rep(0,dm), R[,,t], df=nunew, log=TRUE)-inlik
+      
   }
   
   if((sum(llnew)-sum(llold)+
@@ -129,9 +132,10 @@ for(m in 1:(M+bi)){
   }
   
   tdata  = qt(udata,nuold)
-  B0     = (iota%*%t(iota)-aold%*%t(aold)-bold%*%t(bold))*cov(tdata)
+  Sbar   = cov(tdata)
+  B0     = (iota%*%t(iota)-aold%*%t(aold)-bold%*%t(bold))*Sbar
   
-  resdcc[m,] <- c(aold,bold,nuold) 
+  resdcc[m,] <- c(nuold,aold,bold) 
   Qpred[[m]] <- B0+aold*(tdata[TT,]%*%t(tdata[TT,]))+bold*Qold[,,TT]
   Vpred[[m]] <- diag(diag(Qpred[[m]])^{-1/2})%*%Qpred[[m]]%*%diag(diag(Qpred[[m]])^{-1/2})
   
@@ -141,10 +145,9 @@ for(m in 1:(M+bi)){
     print(Sys.time()-t0)
   }
 }  
+})
 
-
-
-res = list(Vpred[(bi+1):(bi+M)],Qpred[(bi+1):(bi+M)],resdcc[(bi+1):(bi+M),],
-           accdcc[(bi+1):(bi+M)],accnu[(bi+1):(bi+M)])
-names(res) = c('Vpred','Qpred','resdcc','accdcc','accnu')
-save(res,file=paste('empirical/temp/results_vectordcc_tcop.Rdata',sep=''))
+# res = list(Vpred[(bi+1):(bi+M)],Qpred[(bi+1):(bi+M)],resdcc[(bi+1):(bi+M),],
+#            accdcc[(bi+1):(bi+M)],accnu[(bi+1):(bi+M)])
+# names(res) = c('Vpred','Qpred','resdcc','accdcc','accnu')
+# save(res,file=paste('empirical/temp/results_vectordcc_tcop.Rdata',sep=''))
