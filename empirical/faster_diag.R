9bcaa3cf07697fdbf889575ec5f605302a80d9ed
Virbickaitė <audrone.virbickaite@cunef.edu>
2023-12-07 16:02:49 +0100

    k

--- empirical/faster_diag.R
+++ empirical/faster_diag.R
@@ -1,55 +1,31 @@
-M=100000
-dm=15
-
-Qold  = rWishart(1,dm,diag(dm))[,,1]
-data=rnorm(dm)
-l1=l2=l3=rep(NA,M)
-library(profvis)
+rm(list=ls(all=TRUE))
+library(rbenchmark)
 library(Rfast)
+library(mvnfast)
 
 
-x <-rnorm(10) 
-y <-rnorm(10)
-res<-Outer(x,y)
-outer(x,y)
+dm=15
+data=rnorm(dm)
+means=rep(0,dm)
+df=dm+10
+Sigma=rWishart(1,dm+10,diag(dm))[,,1]
 
-# library("microbenchmark")
-# 
-# microbenchmark(mvnfast::dmvt(data,rep(0,dm), R1 , dm+10, log=TRUE), 
-#                Rfast::dmvt(data,rep(0,dm), R1 , dm+10, logged=TRUE),times=1000000)  
-# 
 
-# AUDRA: compare Rfast with mvtnfast for dmvt
-# Rfast is faster
-# use Rfast: dmvt and Outer
+benchmark(Rfast::dmvt(data,means,Sigma,df,logged=TRUE), 
+          mvnfast::dmvt(data,means,Sigma,df,log=TRUE), 
+          order="relative", replications=50000)
 
-fm = diag(dm)
 
-profvis({
-  
-  for(t in 1:M){
-    Ma = Qold 
-    dv <- Ma[ col(Ma)==row(Ma) ]^{-1/2} 
-    R3  <- Outer(dv,dv)*Ma
-    l3[t] = dmvt(data,rep(0,dm), R3 , dm+10, log=TRUE)
-  }
-  
-for(t in 1:M){
-  R1 <- diag(diag(Qold )^{-1/2})%*%Qold %*%diag(diag(Qold )^{-1/2})
-  l1[t] = dmvt(data,rep(0,dm), R1 , dm+10, log=TRUE)
-}
-  
-for(t in 1:M){
-  Ma = Qold 
-  fm[col(fm)==row(fm)] <- Ma[ col(Ma)==row(Ma) ]^{-1/2} 
-  R2  <- fm%*%Ma%*%fm
-  l2[t] = dmvt(data, rep(0,dm), R2 ,dm+10, log=TRUE)
-}
-  
+b=rnorm(dm)
+benchmark(Rfast::Outer(b,b), 
+          base::outer(b,b), 
+          order="relative", replications=50000)
 
-  
-})
 
-sum(l1)
-sum(l2)
-sum(l3)
+# # faster Q matrices
+# for(t in 1:M){
+#   Ma = Qold 
+#   dv <- Ma[ col(Ma)==row(Ma) ]^{-1/2} 
+#   R3  <- Outer(dv,dv)*Ma
+#   l3[t] = dmvt(data,rep(0,dm), R3 , dm+10, log=TRUE)
+# }
