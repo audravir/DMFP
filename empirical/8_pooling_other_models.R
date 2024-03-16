6946b96a86915efaab6c444fc065e594aee65a56
Audra Audra <audrone.virbickaite@gmail.comm>
2024-03-16 07:51:45 +0100

    km

--- empirical/8_pooling_other_models.R
+++ empirical/8_pooling_other_models.R
@@ -61,23 +61,33 @@ mypool = function(ll1,ll2){
 }
 
 best.model = mypool(ll1=lL_caw,ll2=lL_tdcc)
+mat.st <- matrix(rep(lL_static,post.sample), nrow = post.sample, byrow = TRUE)
+mat.rm <- matrix(rep(lL_rmf,post.sample), nrow = post.sample, byrow = TRUE)
+
+
 
 # 1. DCC-t + DCC
 res1 = mypool(ll1=lL_tdcc,ll2=lL_dcc)
 # 2. DCC-t + Static
-mat <- matrix(rep(lL_static,post.sample), nrow = post.sample, byrow = TRUE)
 res2 = mypool(ll1=lL_tdcc,ll2= mat)
 # 3. CAW+AIW
 res3 = mypool(ll1=lL_caw,ll2=lL_xm)
 # 4. CAW+Static
-res4 = mypool(ll1=lL_caw,ll2=mat)
+res4 = mypool(ll1=lL_caw,ll2=mat.st)
 
+# 5. DCC-t + RMf
+res5 = mypool(ll1=lL_tdcc,ll2= mat.rm)
+# 6. CAW+RMf
+res6 = mypool(ll1=lL_caw,ll2=mat.rm)
+
+
+save.image('empirical/temp/other_pools.RData')
 
 move.axis = 100
 mkvol     = apply(tail(apply(RVs^2,2,scale),K),1,mean)
 atx       <- seq(date[(nn+1)], date[(nn+K)], by=25)
 
-pdf('tables_and_figures/other_pools_FX.pdf',height=7,width=14)
+#pdf('tables_and_figures/other_pools_FX.pdf',height=7,width=14)
 par(mfrow=c(2,2), mar=c(4, 3, 1, 1) + 0.1)
 plot(tail(date,K),mkvol,type='l',axes = FALSE,
      col='gray90',lwd=3,ylab='',xlab='',
@@ -144,4 +154,75 @@ axis(1, at=atx, labels=format(atx, "%Y/%m"),las=2,cex.axis=0.75)
 legend(x=date[(nn+1)]-move.axis-35,y=0,col=c(1,'pink','coral'),
        lty=c(1,1,2),lwd=c(2,2,2),
        legend=c('Geweke','Jore1','Equal'))
-dev.off()
+#dev.off()
+
+
+
+
+
+par(mfrow=c(2,2), mar=c(4, 3, 1, 1) + 0.1)
+plot(tail(date,K),mkvol,type='l',axes = FALSE,
+     col='gray90',lwd=3,ylab='',xlab='',
+     xlim=c(date[(nn+1)]-move.axis,date[(nn+K)]),main='DCC-t + DCC')
+par(new = TRUE)
+plot(tail(date,K),rep(0,K),type='l',ylim=c(-600,10),ylab='',xlab='',xaxt="n",
+     xlim=c(date[(nn+1)]-move.axis,date[(nn+K)]))
+lines(tail(date,K),cumsum(apply(res1$lL_gew,2,median))-cumsum(apply(lL_caw,2,median)),
+      col=1,lwd=2)
+lines(tail(date,K),cumsum(apply(res1$lL_jore1,2,median))-cumsum(apply(lL_caw,2,median)),
+      col='pink',lwd=2)
+lines(tail(date,K),cumsum(apply(res1$lL_equal,2,median))-cumsum(apply(lL_caw,2,median)),
+      col='coral',lwd=2,lty=2)
+axis(1, at=atx, labels=format(atx, "%Y/%m"),las=2,cex.axis=0.75)
+legend(x=date[(nn+1)]-move.axis-35,y=0,col=c(1,'pink','coral'),
+       lty=c(1,1,2),lwd=c(2,2,2),
+       legend=c('Geweke','Jore1','Equal'))
+
+plot(tail(date,K),mkvol,type='l',axes = FALSE,
+     col='gray90',lwd=3,ylab='',xlab='',
+     xlim=c(date[(nn+1)]-move.axis,date[(nn+K)]),main='DCC-t + Static')
+par(new = TRUE)
+plot(tail(date,K),rep(0,K),type='l',ylim=c(-750,10),ylab='',xlab='',xaxt="n",
+     xlim=c(date[(nn+1)]-move.axis,date[(nn+K)]))
+lines(tail(date,K),cumsum(apply(res2$lL_gew,2,median))-cumsum(apply(lL_caw,2,median)),
+      col=1,lwd=2)
+lines(tail(date,K),cumsum(apply(res2$lL_jore1,2,median))-cumsum(apply(lL_caw,2,median)),
+      col='pink',lwd=2)
+lines(tail(date,K),cumsum(apply(res2$lL_equal,2,median))-cumsum(apply(lL_caw,2,median)),
+      col='coral',lwd=2,lty=2)
+axis(1, at=atx, labels=format(atx, "%Y/%m"),las=2,cex.axis=0.75)
+legend(x=date[(nn+1)]-move.axis-35,y=0,col=c(1,'pink','coral'),lty=c(1,1,2),lwd=c(2,2,2),
+       legend=c('Geweke','Jore1','Equal'))
+
+plot(tail(date,K),mkvol,type='l',axes = FALSE,
+     col='gray90',lwd=3,ylab='',xlab='',
+     xlim=c(date[(nn+1)]-move.axis,date[(nn+K)]),main='CAW + AIW')
+par(new = TRUE)
+plot(tail(date,K),rep(0,K),type='l',ylim=c(-10,35),ylab='',xlab='',xaxt="n",
+     xlim=c(date[(nn+1)]-move.axis,date[(nn+K)]))
+lines(tail(date,K),cumsum(apply(res3$lL_gew,2,median))-cumsum(apply(lL_caw,2,median)),
+      col=1,lwd=2)
+lines(tail(date,K),cumsum(apply(res3$lL_jore1,2,median))-cumsum(apply(lL_caw,2,median)),
+      col='pink',lwd=2)
+lines(tail(date,K),cumsum(apply(res3$lL_equal,2,median))-cumsum(apply(lL_caw,2,median)),
+      col='coral',lwd=2,lty=2)
+axis(1, at=atx, labels=format(atx, "%Y/%m"),las=2,cex.axis=0.75)
+legend(x=date[(nn+1)]-move.axis-35,y=0,col=c(1,'pink','coral'),lty=c(1,1,2),lwd=c(2,2,2),
+       legend=c('Geweke','Jore1','Equal'))
+
+plot(tail(date,K),mkvol,type='l',axes = FALSE,
+     col='gray90',lwd=3,ylab='',xlab='',
+     xlim=c(date[(nn+1)]-move.axis,date[(nn+K)]),main='CAW + RMf')
+par(new = TRUE)
+plot(tail(date,K),rep(0,K),type='l',ylim=c(-200,100),ylab='',xlab='',xaxt="n",
+     xlim=c(date[(nn+1)]-move.axis,date[(nn+K)]))
+lines(tail(date,K),cumsum(apply(res4$lL_gew,2,median))-cumsum(apply(lL_caw,2,median)),
+      col=1,lwd=2)
+lines(tail(date,K),cumsum(apply(res4$lL_jore1,2,median))-cumsum(apply(lL_caw,2,median)),
+      col='pink',lwd=2)
+lines(tail(date,K),cumsum(apply(res4$lL_equal,2,median))-cumsum(apply(lL_caw,2,median)),
+      col='coral',lwd=2,lty=2)
+axis(1, at=atx, labels=format(atx, "%Y/%m"),las=2,cex.axis=0.75)
+legend(x=date[(nn+1)]-move.axis-35,y=0,col=c(1,'pink','coral'),
+       lty=c(1,1,2),lwd=c(2,2,2),
+       legend=c('Geweke','Jore1','Equal'))
